2f6d3d088ebc056d419adef10f7e0d915a87abd9
Douglas Rosenberg <doug.rosenberg@gmail.com>
2022-10-19 16:21:54 -0500

    initial

---
+++ API/TheDevBlog/TheDevBlog.API/Data/TheDevBlogDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TheDevBlog.API.Models;
+
+namespace TheDevBlog.API.Data
+{
+    public class TheDevBlogDbContext : DbContext
+    {
+        public TheDevBlogDbContext(DbContextOptions<TheDevBlogDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Post> Posts { get; set; }
+    }
+}
